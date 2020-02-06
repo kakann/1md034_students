9f254028faa563bca3f94fa95d67eb05b50733c9
kakann <nitram.eriksson@hotmail.com>
2020-02-06 13:26:21 +0100

    MESSAGE

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -35,11 +35,12 @@ const vm = new Vue({
 })
 */
 
+'use strict';
+const socket = io();
+
 function customer(name, email, adress, house, gender, payOption){
     this.name= name;
     this.email= email;
-    this.adress = adress;
-    this.house = house;
     this.gender= gender;
     this.payOption = payOption;    
 }
@@ -55,15 +56,59 @@ const vn = new Vue({
         copiedBurgers: food,
         burgArray: [],
         displayArray: false,
-        house: ""
+        house: "",
+        orders: {},
     },
+    created: function() {
+    /* When the page is loaded, get the current orders stored on the server.
+     * (the server's code is in app.js) */
+    socket.on('initialize', function(data) {
+      this.orders = data.orders;
+    }.bind(this));
+
+    /* Whenever an addOrder is emitted by a client (every open map.html is
+     * a client), the server responds with a currentQueue message (this is
+     * defined in app.js). The message's data payload is the entire updated
+     * order object. Here we define what the client should do with it.
+     * Spoiler: We replace the current local order object with the new one. */
+    socket.on('currentQueue', function(data) {
+      this.orders = data.orders;
+    }.bind(this));
+  },
     methods: {
+        getNext: function() {
+      /* This function returns the next available key (order number) in
+       * the orders object, it works under the assumptions that all keys
+       * are integers. */
+      let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
+        return Math.max(last, next);
+      }, 0);
+      return lastOrder + 1;
+    },
+    addOrder: function(event) {
+      /* When you click in the map, a click event object is sent as parameter
+       * to the function designated in v-on:click (i.e. this one).
+       * The click event object contains among other things different
+       * coordinates that we need when calculating where in the map the click
+       * actually happened. */
+      let offset = {
+        x: event.currentTarget.getBoundingClientRect().left,
+        y: event.currentTarget.getBoundingClientRect().top,
+      };
+      socket.emit('addOrder', {
+        orderId: this.getNext(),
+        details: {
+          x: event.clientX - 10 - offset.x,
+          y: event.clientY - 10 - offset.y,
+        },
+        orderItems: ['Beans', 'Curry'],
+      });
+    },
         markDone: function() {
             this.displayArray=true;
             var firstAndLast = document.getElementById("firstname").value;
             var email = document.getElementById("email").value;
-            var adress = document.getElementById("adress").value;
-            var housenr = document.getElementById("housenr").value;
+            
             var gender = "male";
 
             if(document.getElementById("male").checked){
@@ -80,14 +125,9 @@ const vn = new Vue({
             var selectedValue = payOption.options[payOption.selectedIndex].value;
             
 
-            let customerrr = new customer(firstAndLast, email, adress, housenr, gender, selectedValue);
-
-            console.log( housenr );
+            let customerrr = new customer(firstAndLast, email, gender, selectedValue);
            
-
             document.getElementById("cname").innerHTML = "Name: " + customerrr.name;
-            document.getElementById("cadress").innerHTML = "Adress: " + customerrr.adress;
-            document.getElementById("chouse").innerHTML = "House number: " + housenr;
             document.getElementById("cgender").innerHTML = "Gender: "+ customerrr.gender;
             document.getElementById("cpayment").innerHTML = "Choosen payment method: "+ customerrr.payOption;
 
